68444953b4fdfa9856add25d8c6a71cfee19098b
chenting <chenting@rinnai.com.cn>
2021-07-01 09:06:11 +0800

    3

---
+++ TestSupportEnvDemo/Base/environment_preprocess.h
@@ -0,0 +1,8 @@
+//-----------------------------------------
+// Auto generated file
+// Created 2021-07-01 09:04:57
+//-----------------------------------------
+
+#define RN_ENVIRONMENT              DEV
+#define RN_ENVIRONMENT_APP_NAME     林内智家 01
+#define RN_ENVIRONMENT_LAST_COMMIT  .1
---
+++ TestSupportEnvDemo/environment_preprocess.h
@@ -0,0 +1,11 @@
+//-----------------------------------------
+// Auto generated file
+// Created 2021-06-29 16:20:17
+//-----------------------------------------
+
+#define NWD_ENVIRONMENT              TEST
+#define NWD_ENVIRONMENT_APP_NAME     林内智家 29
+#define NWD_ENVIRONMENT_LAST_COMMIT  .1
+#define NWD_SHARE_SCHEME_WB  
+#define NWD_SHARE_SCHEME_QQ  
+#define NWD_SHARE_SCHEME_WX  
